705caa5fa51bf1a831621cd393476b0b1e739ea6
paulleflanchec-deliverycar <paul.le-flanchec@deliverycar.fr>
2026-03-03 08:37:45 +0100

    Add sha256

--- Formula/delivery-car-setup.rb
+++ Formula/delivery-car-setup.rb
@@ -2,7 +2,7 @@ class DeliveryCarSetupDev < Formula
   desc "Delivery Car full dev environment setup"
   homepage "https://deliverycar.fr"
   url "https://github.com/deliverycar/homebrew-deliverycar/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "SHA256_A_REMPLACER"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "1.0.0"
 
   depends_on "git"
